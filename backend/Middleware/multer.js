7ed96cc1fe5548f7c20081b2d71c618725f85d5c
renjithps <renjith.psirohub.com>
2026-01-06 10:06:17 +0530

    file size increased

--- backend/Middleware/multer.js
+++ backend/Middleware/multer.js
@@ -18,7 +18,7 @@ const upload = multer({
     storage,
     fileFilter,
     limits: {
-        fileSize: 5 * 1024 * 1024, // 5MB per file
+        fileSize: 10 * 1024 * 1024, // 5MB per file
     }
 });
 
